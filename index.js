f427c3398850469baccb00b69d6b82770ef5ba88
AnthonyC233 <changanthony61@yahoo.com>
2022-04-15 22:01:19 -0400

    Moved code into generateMardown.js and added const

--- index.js
+++ index.js
@@ -2,88 +2,58 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
 
-const writeFile = (answers) =>
-`
-# ${answers.title}
-[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
-
-
-## ${answers.Description}
-
-## Table of Contents
-1.[Installation](#Installation)
-2.[Usage](#Usage)
-3.[Liscense](#Liscense)
-4.[Liscense](#Contributions)
-5.[Tests](#Tests)
-6.[GitHub](#GitHub)
-
-
-## ${answers.Installation}
-
-## ${answers.Usage}
-
-## ${answers.Liscense}
-
-## ${answers.Contributions}
-
-## ${answers.Tests}
-
-## ${answers.GitHub}
-
-`
 // Inqurier download from npm, made prompt and etc.
-inquirer
-    .prompt ([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is your title?',
-        },
-        {
-            type: 'input',
-            name: 'Description',
-            message: 'What is your description?',
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'Enter installation instructions',
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'How is it used?',
-        },
-        {
-            type: 'list',
-            name: 'Liscense',
-            message: 'Choose a liscense',
-            choices: ['Rust', 'MIT', 'GNU'],
-        },
-        {
-            type: 'input',
-            name: 'Contributions',
-            message: 'How can I use your code?',
-        },
-        {
-            type: 'input',
-            name: 'Tests',
-            message: '',
-        },
-        {
-            type: 'input',
-            name: 'GitHub',
-            message: 'Enter GitHub Profile',
-        },
-    ])
-    // Creates write file to create README.md file
-    .then((answers) => {
-        const pikachuContent = writeFile(answers);
-
-        fs.writeFile('README.md', pikachuContent, (err) =>
-            err ? console.log(err) : console.log('Succesfully created index.hmtl')
-        );
-    });
-
+function init () {
+    inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is your title?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'What is your description?',
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Enter installation instructions',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'How is it used?',
+            },
+            {
+                type: 'list',
+                name: 'liscense',
+                message: 'Choose a liscense',
+                choices: ['Apache', 'MIT', 'GNU'],
+            },
+            {
+                type: 'input',
+                name: 'contributions',
+                message: 'How can I use your code?',
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: '',
+            },
+            {
+                type: 'input',
+                name: 'GitHub',
+                message: 'Enter GitHub Profile',
+            },
+        ])
+        // Creates write file to create README.md file
+        .then((answers) => {
+            fs.writeFile('README.md', generateMarkdown(answers), (err) =>
+                err ? console.log(err) : console.log('Succesfully created README.md')
+            );
+        });
+}
+init();
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,46 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicense(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+function renderLicense(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GNU') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } 
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(answers) {
+  const badge = renderLicense(answers.liscense)
+  return `
+    # ${answers.title} ${badge}
+  
+    ## ${answers.description}
+  
+    ## Table of Contents
+    1.[Installation](#Installation)
+    2.[Usage](#Usage)
+    3.[Liscense](#Liscense)
+    4.[Liscense](#Contributions)
+    5.[Tests](#Tests)
+    6.[GitHub](#GitHub)
+    
+    
+    ## ${answers.installation}
+    
+    ## ${answers.usage}
+    
+    ## ${answers.liscense}
+    
+    ## ${answers.contributions}
+    
+    ## ${answers.tests}
+    
+    ## ${answers.GitHub}
+    
+    `
+  ;
 }
+
 
 module.exports = generateMarkdown;
